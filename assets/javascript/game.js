ebf6cb1a7d017ec4b9417fe23a0aa3a0b8d524d4
Stepan <stepos36@gmail.com>
2018-11-17 00:13:36 -0500

    looking for solution for wrongWord array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,40 +3,46 @@ var words = ['LOST', 'Dexter', 'Breaking Bad', 'Big bang theory', 'Scrubs', 'Wes
 var randNum = Math.floor(Math.random() * words.length)
 var chosenWord = words[randNum]
 var underScore = []
+var rightWord = []
 var wrongWord = []
+var objUnderScore = document.getElementsByClassName("underscore")
+var objWrongWord = document.getElementsByClassName("wrongletters")
 console.log(chosenWord)
 //Generate a required amound of underscores in the letter row
 function generateUnderscores() {
     for (var i=0; i<chosenWord.length;i++) {
-        underScore[i] = ("_")
+        underScore[i] = "_"
     }
     return underScore;
 }
-//DOM manipulation
-var objUnderScore = document.getElementsByClassName("underscore")
-var objWrongWord = document.getElementsByClassName("wrongletters")
+
 //Pick up the key user pressed
 document.addEventListener('keypress', function(event) {
    var keyCode = event.keyCode;
    var keyWord = String.fromCharCode(keyCode);
    console.log(keyWord)
 //Compare the key user pressed with the word we have
-    //if the letter user entered is in the chosenWord
-    if (chosenWord.indexOf(keyWord) > -1) {
-        //then replace an underscore with the same index with this letter
-        underScore[chosenWord.indexOf(keyWord)] = keyWord
-        
+    for (var j=0; j<chosenWord.length;j++) {
+        if (chosenWord[j] === keyWord) {
+        //Add our letter to the right guess array
+            
             
         //Replace the underscore with the right key
-            
+            underScore[j] = keyWord;
             objUnderScore[0].innerHTML = underScore.join(' ')
            
         }
-        else {
-            wrongWord.push(keyWord);
-            objWrongWord[0].innerHTML = wrongWord.join(' ');
+        else if (chosenWord[j] !== keyWord) {
+            function wrongLetter() {
+                wrongWord.push(keyWord);
+                objWrongWord[0].innerHTML = wrongWord.join(' ')
+                console.log(wrongWord)}
+        };
+        
             
-            };
+        
+    }
+    wrongLetter()
 }) 
 
 //Add the wrong letter to the wrong letter section
